c7fffdf8414e9c7b94918fbf091d95a73e7d9a15
Van Nguyen <99274250+van-nguyen2006@users.noreply.github.com>
2022-04-22 23:41:30 +0000

    Added IF statement for if the user inputs 0.

--- js/script.js
+++ js/script.js
@@ -13,20 +13,32 @@ function calculate () {
   let units = document.getElementById('units').value;
 
   // Error Message for Invalid Unit entry
-  let errorMessage = "Invalid Units";
+  let invalidUnitMsg = "Invalid Units";
+  let zeroInputMsg = "Zero is not a valid number! (results to nothing)"
 
   // Process (calculates Volume and Surface Area)
   let volume = ((3 * Math.sqrt(3)) / 2) * (baseEdge ** 2) * height;
   let surfaceArea = 6 * baseEdge * height + 3 * Math.sqrt(3) * (baseEdge ** 2);
 
   // Outputs Volume and Surface Area with IF statement to ensure that user entered a metric unit
-  if (units === "mm" || units === "cm" || units === "dm" || units === "m" || units === "dam" || units === "hm" || units === "km") {
-    document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + units + "³";
-    document.getElementById('surface-area').innerHTML = 'Surface Area is: ' + surfaceArea.toFixed(2) + units + "²";
-    document.getElementById('error-message').innerHTML = "";
-  } else {
-    document.getElementById('error-message').innerHTML = errorMessage;
+  if (height === 0 || baseEdge === 0) {
+    document.getElementById('zero-input').innerHTML = zeroInputMsg;
     document.getElementById('volume').innerHTML = "";
     document.getElementById('surface-area').innerHTML = "";
+    document.getElementById('invalid-unit').innerHTML = "";
+  } else if (units === "mm" || units === "cm" || units === "dm" || units === "m" || 
+      units === "dam" || units === "hm" || units === "km") {
+    document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + 
+      units + "³";
+    document.getElementById('surface-area').innerHTML = 'Surface Area is: ' + 
+      surfaceArea.toFixed(2) + units + "²";
+    document.getElementById('invalid-unit').innerHTML = "";
+    document.getElementById('zero-input').innerHTML = "";
+  } else {
+    document.getElementById('invalid-unit').innerHTML = invalidUnitMsg;
+    document.getElementById('volume').innerHTML = "";
+    document.getElementById('surface-area').innerHTML = "";
+    document.getElementById('zero-input').innerHTML = "";
   }
 }
+
