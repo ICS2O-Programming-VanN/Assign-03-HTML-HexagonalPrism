6c8e2ce1c3b87dd13bda3ea0669b5ed4d1b1082c
Van Nguyen <99274250+van-nguyen2006@users.noreply.github.com>
2022-04-27 22:26:48 +0000

    Added more comments, altered first IF statement (that ensures complete input).

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function calculate () {
   let height = parseFloat(document.getElementById('height').value); // Stores value from user
   let units = document.getElementById('units').value;
 
-  // Error Message for Invalid Unit entry
+  // Error Messages for Invalid Unit entry or input of 0
   let invalidUnitMsg = "Invalid Units";
   let zeroInputMsg = "Zero is not a valid number! (results to nothing)"
 
@@ -20,14 +20,16 @@ function calculate () {
   let volume = ((3 * Math.sqrt(3)) / 2) * (baseEdge ** 2) * height;
   let surfaceArea = 6 * baseEdge * height + 3 * Math.sqrt(3) * (baseEdge ** 2);
 
-  if (baseEdge > 0 && height > 0 && units != "") {
-    // Outputs Volume and Surface Area with IF statement to ensure that user entered a metric unit
+  // Checks to make sure that user made input in all fields
+  if (baseEdge >= 0 && height >= 0 && units != "") {
+    // If user entered 0 for height or base edge length
     if (height === 0 || baseEdge === 0) {
       document.getElementById('zero-input').innerHTML = zeroInputMsg;
       document.getElementById('volume').innerHTML = "";
       document.getElementById('surface-area').innerHTML = "";
       document.getElementById('invalid-unit').innerHTML = "";
       document.getElementById('empty-field').innerHTML = "";
+      // If they entered a proper metric unit
     } else if (units === "mm" || units === "cm" || units === "dm" || units === "m" || 
         units === "dam" || units === "hm" || units === "km") {
       document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + 
@@ -37,6 +39,7 @@ function calculate () {
       document.getElementById('invalid-unit').innerHTML = "";
       document.getElementById('zero-input').innerHTML = "";
       document.getElementById('empty-field').innerHTML = "";
+      // If the user did not enter valid units
     } else {
       document.getElementById('invalid-unit').innerHTML = invalidUnitMsg;
       document.getElementById('volume').innerHTML = "";
@@ -44,6 +47,7 @@ function calculate () {
       document.getElementById('zero-input').innerHTML = "";
       document.getElementById('empty-field').innerHTML = "";
     }
+    // If user did not a value in all the input fields
   } else {
     document.getElementById('empty-field').innerHTML = "Please enter ALL fields!";
     document.getElementById('volume').innerHTML = "";
